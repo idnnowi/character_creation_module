d8e10c57398d6da1c08dc5ef2ae82255583b4c52
Vladislav Konovalov <idnno@mail.ru>
2022-12-28 16:53:49 +0300

    Code according to pep8

--- test_module.py
+++ test_module.py
@@ -1,13 +1,13 @@
 import os
 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
+BASE_DIR = os.path.dirchar_name(os.path.abspath(__file__))
+dir_files = [filechar_name.lower() for filechar_name in os.listdir(BASE_DIR)]
 
 files_list = ['main.py', 'readme.md']
 
 
 def test_program():
-    for filename in files_list:
-        assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
-
+    for filechar_name in files_list:
+        assert filechar_name in dir_files, (f'Файл `{filechar_name}` '
+                                            f'не найден в корне репозитория')
